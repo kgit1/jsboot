32dc55c5ade8d2540a821e46923a7e38e26ab858
kgit1 <kgit1@rambler.ru>
2018-05-12 08:44:12 +0300

    js start

---
+++ firstPage/scriptDemo.js
@@ -0,0 +1,8 @@
+
+var userName = prompt("what is your name");
+alert("Hello "+userName+", from a js file");
+console.log("Hello "+userName+", in the console from a js file");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+console.log("User profile:\nName: " + userName + "\nLastName: " + userLastName 
+	+ "\nAge: " + userAge + "\n" + userAge + "years = " + userAge*365.25 + " days");
